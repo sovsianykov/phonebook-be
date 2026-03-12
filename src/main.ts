013a37adc69ef5e8d983dd4cbbc8c88ae38a35b1
sovsianykov <serhii.ovsianykov@dataart.com>
2026-03-12 12:55:58 +0200

    fixes 3

--- src/main.ts
+++ src/main.ts
@@ -33,6 +33,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(process.env.PORT ?? 8080);
+  await app.listen(process.env.PORT || 8080);
 }
 void bootstrap();
